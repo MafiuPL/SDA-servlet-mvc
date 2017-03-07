552732e2790f8506e0a98c278420c4e5664f4bd7
unknown <mafiupl@gmail.com>
2017-03-07 21:00:52 +0100

    Updated Hello servlet to receive posts with user data

--- src/main/java/com/mateusz/servlet/HelloServlet.java
+++ src/main/java/com/mateusz/servlet/HelloServlet.java
@@ -3,6 +3,8 @@ package com.mateusz.servlet;
 import com.mateusz.controller.Controller;
 import com.mateusz.controller.CookieController;
 import com.mateusz.controller.UserController;
+import file.FileOperations;
+import file.User;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.naming.ldap.Control;
@@ -11,6 +13,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -44,5 +48,22 @@ public class HelloServlet extends HttpServlet {
             controller = controllerMap.get("default");
         }
         controller.handleGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+        String body = reader.readLine();
+        String[] split = body.split(" ");
+        User user = new User();
+        user.setFirstName(split[0]);
+        user.setLastName(split[1]);
+        user.setAge(new Integer(split[2]));
+
+        File file = new File("C:\\Users\\RENT\\Desktop\\test.txt");
+        FileOperations.addUserToFile(user, file);
+
+        resp.getWriter().write("Wszystko ok");
+        resp.setStatus(201);
     }
 }
